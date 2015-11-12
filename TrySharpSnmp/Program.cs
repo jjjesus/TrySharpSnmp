0cfe056ad2a5004bb1bbf7646a8d9b2b1d874dd4
John Jesus <john.jesus@raytheon.com>
2015-11-12 13:17:42 -0600

    Initial commit.

---
+++ TrySharpSnmp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySharpSnmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.WriteLine("Hit ENTER to continue");
+            Console.ReadLine();
+        }
+
+        static void Run()
+        {
+            SnmpWorker worker = new SnmpWorker();
+            worker.IPAddr = "192.168.10.6";
+            worker.PortNum = 161;
+            worker.CommunityName = "public";
+            worker.TimeoutMsec = 60000;
+
+            worker.Get(".1.3.6.1.2.1.1.1.0");
+        }
+    }
+}
---
+++ TrySharpSnmp/SnmpWorker.cs
@@ -0,0 +1,42 @@
+﻿using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySharpSnmp
+{
+    public class SnmpWorker
+    {
+        public string IPAddr { get; set; }
+        public int PortNum { get; set; }
+        public string CommunityName { get; set; }
+        public int TimeoutMsec { get; set; }
+
+        public void Get(string oid)
+        {
+            IList<Variable> results = Messenger.Get(
+                VersionCode.V2,
+                new IPEndPoint(IPAddress.Parse(this.IPAddr), this.PortNum),
+                new OctetString(this.CommunityName),
+                new List<Variable> {
+                    new Variable(
+                        new ObjectIdentifier(oid))
+                },
+                this.TimeoutMsec);
+            DumpResults(results);
+        }
+
+        private void DumpResults(IList<Variable> results)
+        {
+            foreach (Variable result in results)
+            {
+                Console.WriteLine(result.Data.ToString());
+            }
+        }
+
+    }
+}
